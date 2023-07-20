3c292fb5898b08438bb21ab5cb4a6783e245911e
Amir Mughal <amirmughal2009@gmail.com>
2023-07-20 23:53:48 +0500

    tailwind css integration

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div>
+        <div class="text-xl font-medium text-black">Tailwind Css Integration</div>
+      </div>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
